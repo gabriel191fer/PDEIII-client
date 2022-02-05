a7458b5b0e31127ff10de1931a4550f3ea0f3903
Gabriel Fernandes <gabriel191fernandes@live.com>
2022-02-05 09:48:19 -0300

    added initial screen interface

--- App.tsx
+++ App.tsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import { Button, Image, View, Platform, TouchableOpacity, Text } from 'react-native';
+import { Button, Image, View, Platform, TouchableOpacity, Text, StyleSheet, Pressable } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
+
+const axios = require('axios').default;
 
 export default function ImagePickerExample() {
   const [image, setImage] = useState(null);
@@ -15,6 +17,7 @@ export default function ImagePickerExample() {
     });
 
     console.log(result);
+    alert('Imagem selecionada com Sucesso!')
 
     if (!result.cancelled) {
       setImage(result.uri);
@@ -27,40 +30,75 @@ export default function ImagePickerExample() {
       // If file selected then create FormData
       const fileToUpload = image;
       const data = new FormData();
-      data.append('image', 'Image Upload');
       data.append('image', fileToUpload);
 
-      let adress = 'http://tomatoapi-env.eba-pnwe8e3p.us-east-1.elasticbeanstalk.com/leaf'
-      let res = await fetch(
-        adress,
-        {
-          method: 'post',
-          body: data,
-          headers: {
-            'Content-Type': 'multipart/form-data; ',
-          },
-        }
-      );
-      let responseJson = await res.json();
-      if (responseJson.status == 1) {
-        alert('Carregamento concluído');
-      }
+      const test = await axios.post('http://tomatoapi-env.eba-pnwe8e3p.us-east-1.elasticbeanstalk.com/leaf',{
+        data: data,
+        headers: { "Content-Type": "multipart/form-data" },
+      });
+
+      console.log('dados : ', test)
+
+      // let adress = 'http://172.20.0.3:5000/'
+      // let res = await fetch(
+      //   adress,
+      //   {
+      //     method: 'post',
+      //     body: data,
+      //     headers: {
+      //       'Content-Type': 'multipart/form-data; ',
+      //     },
+      //   }
+      // );
+      // let responseJson = await res.json();
+      // if (responseJson.status == 1) {
+      //   alert('Carregamento concluído');
+      // }
     } else {
       alert('Por favor Selecione um arquivo');
     }
   };
 
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Button title="Selecionar Imagem" onPress={pickImage} />
-      {image && <Image source={{ uri: image }} style={{ width: 256, height: 256 }} />}
+    <View>
 
-      <Button
-        onPress={uploadImage}
-        title='Subir Image'>
-      </Button>
+      
+    <Text style={{
+          marginTop:15,
+          textAlign:'center',
+          fontSize:25,
+          backgroundColor: 'gray',
+          paddingBottom: 10,
+          paddingTop: 10
+        }}>
+          Analisador de Tomates
+        </Text>
+        <Image source={require('./images/tomato.png')} style={{ height: 300, width: 300, alignSelf: 'center', marginTop: 50}} />
+    
+      
+
+      <Pressable style={{ width: 120, height:40, backgroundColor: 'gray', alignSelf:'center', borderRadius: 5, marginBottom: 10, marginTop: 50}} onPress={pickImage}>
+          <Text style={{ textAlign: 'center' }}>
+            Selecionar Imagem
+          </Text>
+      </Pressable>
+      {/*{image && <Image source={{ uri: image }} style={{ width: 256, height: 256 }} />} */}
+
+      <Pressable style={{ width: 120, height:35, backgroundColor: 'gray', alignSelf:'center', borderRadius: 5, padding:6}}
+        onPress={uploadImage}>
+        <Text style={{ textAlign: 'center' }}>Subir Imagem</Text>
+      </Pressable>
+
+      <Text style={{textAlign: 'center', marginTop: 110}}>
+        Powered by m0b1us
+      </Text>
 
 
-    </View>
+    
+    
+  </View>
+    
   );
+
+  
 }
